a8421522ba65d671c321ef27e1e617dd5d2c2c33
Daniel Pereira <54162368+Dannyp425@users.noreply.github.com>
2021-02-28 20:57:24 -0700

    80 %

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,64 +1,80 @@
 var events = {};
-var hour = ["9", "10", "11", "12", "13", "14", "15", "16", "17", "20"]
-var weekDay =  "";
-    switch(dayjs().day()) {
-      case 0: 
+
+var weekDay = "";
+switch (dayjs().day()) {
+    case 0:
         weekDay = "Sunday";
         break;
-    
-      case 1: 
+
+    case 1:
         weekDay = "Monday";
         break;
 
-      case 2: 
+    case 2:
         weekDay = "Tuesday";
         break;
 
-      case 3: 
+    case 3:
         weekDay = "Wednesday";
         break;
 
-      case 4: 
+    case 4:
         weekDay = "Thursday";
         break;
 
-      case 5: 
+    case 5:
         weekDay = "Friday";
         break;
 
-      case 6: 
+    case 6:
         weekDay = "Saturday";
         break;
-    }
+}
 var monthDay = dayjs().format("MMMM D");
 var currDay = weekDay + ", " + monthDay + "th"
-    
+
 $("#currentDay").text(currDay);
 
-var eventText = $(" input[type=text][name=9am] ").val();
 
-$(".save-btn").on("click", function() {
+$(".9btn").on("click", function() {
+    console.log("9 am");
+})
+$(".10btn").on("click", function() {
+    console.log("10 am");
+})
+$(".save-btn").on("click", function () {
     //change it so that it saves the text for specific hour codes
     saveEvents(eventText);
 });
 
-function saveEvents () {
+function saveEvents() {
     //save events to localStorage
     localStorage.setItem("events", JSON.stringify(events));
 };
 
-var loadEvents = function() {
+var loadEvents = function () {
     //display events stored in localStorage
     events = JSON.parse(localStorage.getItem("events"));
-  
+
     if (!events) {
-      events = {
-        hrcode: [],
-        name: [],
-      };
+        events = {
+            hrcode: [],
+            name: [],
+        };
     };
 };
 
 function colorCode() {
-    //use input name to check against current hour
+    $("textarea").each(function (index, element) {
+        var currHr = dayjs().hour();
+        if (element.id < currHr) {
+            $(element).addClass("past");
+        } else if (element.id == currHr) {
+            $(this).addClass("present");
+        } else {
+            $(element).addClass("future");
+        }
+    });
 }
+
+colorCode();
